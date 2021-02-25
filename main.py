4a577c2b81201503676d34509524f99a424fbf82
Nahuel <nahuel.camillo@gmail.com>
2021-02-25 20:24:06 -0300

    Init project

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+import lxml
+import time
+import sys
+
+from datetime import datetime
+
+store = 'https://www.playstation.com/en-us/ps-plus/this-month-on-ps-plus/'
+container = '//*[contains(@class, "text-block")]/text()'
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
