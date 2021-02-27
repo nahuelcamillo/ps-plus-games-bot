9cbf264eb68ba56e744cc85ba39be1317a0c35f4
Nahuel <nahuel.camillo@gmail.com>
2021-02-27 11:16:00 -0300

    lxml implementation and first scrapping of ps-plus games

--- main.py
+++ main.py
@@ -6,13 +6,33 @@ import lxml
 import time
 import sys
 
+from lxml import html
+from datetime import datetime
+
 TOKEN = '<token string>'
 # tb = telebot.TeleBot(TOKEN)
 
-store = 'https://www.playstation.com/en-us/ps-plus/this-month-on-ps-plus/'
-container = '//*[contains(@class, "text-block")]/text()'
+url = 'https://www.playstation.com/en-us/ps-plus/this-month-on-ps-plus/'
+# container = '//*[contains(@class, "text-block")]/text()'
+container = "//div/h3/text()"
 
 def main():
+    start_time = time.time()
+
+    results = scrapping()
+    print (results["items"])
+
+def scrapping():
+    items = []
+
+    # print("URL Scrapped: " + url)
+    xpath = container
+    html = requests.get(url)
+    doc = lxml.html.fromstring(html.content)
+    items = doc.xpath(xpath)
+    # items = doc.xpath("//div/h3/text()")
+
+    return { "items": items }
 
 
 if __name__ == '__main__':
