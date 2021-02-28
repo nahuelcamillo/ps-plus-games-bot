6bab0919113ead390607b50ffbc5355e204f3d4b
Nahuel <nahuel.camillo@gmail.com>
2021-02-28 20:55:24 -0300

    Obtain links and filter list of items and links

--- main.py
+++ main.py
@@ -15,25 +15,27 @@ TOKEN = '<token string>'
 url = 'https://www.playstation.com/en-us/ps-plus/this-month-on-ps-plus/'
 # container = '//*[contains(@class, "text-block")]/text()'
 container = "//div/h3/text()"
+link = '//*[contains(@class, "buttonblock")]//a/@href'
 
 def main():
     start_time = time.time()
 
     results = scrapping()
-    print (results["items"])
+    print (results["items"][0:3])
+    print (results["links"][0:3])
 
 def scrapping():
     items = []
 
-    # print("URL Scrapped: " + url)
-    xpath = container
+    print("URL Scrapped: " + url)
+    xpathItems = container
+    xpathLinks = link
     html = requests.get(url)
     doc = lxml.html.fromstring(html.content)
-    items = doc.xpath(xpath)
-    # items = doc.xpath("//div/h3/text()")
+    items = doc.xpath(xpathItems)
+    links = doc.xpath(xpathLinks)
 
-    return { "items": items }
-
+    return { "items": items, "links": links }
 
 if __name__ == '__main__':
     main()
