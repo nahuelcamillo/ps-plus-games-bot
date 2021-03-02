4f803a2a78ff62ae04de0a17b64145951feef9e6
Nahuel <nahuel.camillo@gmail.com>
2021-03-02 09:00:58 -0300

    Trying to implement json.loads to obtain chatIDs from external json file

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ from lxml import html
 from datetime import datetime
 
 url = 'https://www.playstation.com/en-us/ps-plus/this-month-on-ps-plus/'
+month = '//*[contains(@class, "txt--6")]//span/text()'
 container = "//div/h3/text()"
 link = '//*[contains(@class, "buttonblock")]//a/@href'
+monthCompare = 'PlayStation Plus: February 2021'
+
 
 def main():
     
     bot_token = sys.argv[1]
+    # recipients = json.loads(sys.argv[2])
     recipients = sys.argv[2]
     print(recipients)
 
@@ -28,13 +32,23 @@ def main():
 
     games = results["items"][0:3]
     links = results["links"][0:3]
+    currentMonth = results["currentMonth"][0]
 
-    if results["items"]:
-        message = "🎮 📆 These are the free games of the month on PS Plus:\n\n👾 " + "\n👾 ".join(games) + "\n\n🔗 " + '\n🔗 '.join(links)
+    if currentMonth == monthCompare:
+        print("Iguales")
+        if results["items"]:
+            message = "🎮 📆 These are the free games of " + currentMonth + ":\n\n👾 " + "\n👾 ".join(games) + "\n\n🔗 " + '\n🔗 '.join(links)
 
-    print(message)
+        print(message)
 
-    telegram_bot_sendtext(message, bot_token, recipients)
+        telegram_bot_sendtext(message, bot_token, recipients)
+    else:
+        if results["items"]:
+            message = "🎮 📆 These are the free games of " + currentMonth + ":\n\n👾 " + "\n👾 ".join(games) + "\n\n🔗 " + '\n🔗 '.join(links)
+
+        print(message)
+
+        telegram_bot_sendtext(message, bot_token, recipients)
 
 def scrapping():
     items = []
@@ -42,12 +56,15 @@ def scrapping():
     print("URL Scrapped: " + url)
     xpathItems = container
     xpathLinks = link
+    xpathMonth = month
     html = requests.get(url)
     doc = lxml.html.fromstring(html.content)
     items = doc.xpath(xpathItems)
     links = doc.xpath(xpathLinks)
+    monthTitle = doc.xpath(xpathMonth)
+    print(monthTitle)
 
-    return { "items": items, "links": links }
+    return { "items": items, "links": links, "currentMonth": monthTitle }
 
 def telegram_bot_sendtext(bot_message, bot_token, recipients):
     # for bot_chatID in recipients:
